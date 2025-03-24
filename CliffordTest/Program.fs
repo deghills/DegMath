c5b5f68ec7a9fa85d249f856f5990f42abe09dea
5sf <atticusozfinch@gmail.com>
2025-03-24 00:05:05 -0700

    Added all of the unit tests that I've verified (work in progress)

--- CliffordTest/Program.fs
+++ CliffordTest/Program.fs
@@ -1,2 +1,39 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open DegMath
+open Clifford
+open Multivector
+
+let testMultivectorPrint(m:Multivector) =
+    Multivector.print m
+
+let testMultivectorOfBlade =
+    Multivector.ofBlade >> Multivector.print
+
+let testMultivectorOfBlades : Blade seq -> unit =
+    Multivector.ofBlades >> Multivector.print
+
+let testVectors(p, q, n) =
+    let cl = Cl(p, q, n)
+    let vecs =
+        cl.Vectors
+        |> Seq.map (
+            function KeyValue(potency, vecs) ->
+                        vecs 
+                        |> Seq.map (fun v -> potency, v)) 
+                        |> Seq.collect id
+    for (potency, v) in vecs do printfn $"{(potency, v)}"
+
+let testBivectors(p, q, n) =
+    for bv in Cl(p, q, n).Bivectors do printfn "%A" bv
+
+
+[<EntryPoint>]
+let main _ =
+    let blades = [
+        0b10uy, 10f
+        0b01uy, System.MathF.PI
+        0uy, 1f
+        0b11uy, 99f
+    ]
+    do testMultivectorOfBlades blades
+
+    0
