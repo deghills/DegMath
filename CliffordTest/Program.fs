1a339508dba4b20992019834ec4bb8f9587d947d
5sf <atticusozfinch@gmail.com>
2025-03-24 07:22:15 -0700

    Committed all unit tests that have been tested

--- CliffordTest/Program.fs
+++ CliffordTest/Program.fs
@@ -1,6 +1,6 @@
-﻿open DegMath
+﻿open System
+open DegMath
 open Clifford
-open Multivector
 
 let testMultivectorPrint(m:Multivector) =
     Multivector.print m
@@ -10,6 +10,9 @@ let testMultivectorOfBlade =
 
 let testMultivectorOfBlades : Blade seq -> unit =
     Multivector.ofBlades >> Multivector.print
+
+let testGetBlade : byte -> Multivector -> unit =
+    fun b m -> Multivector.getBlade b m |> Console.WriteLine
 
 let testVectors(p, q, n) =
     let cl = Cl(p, q, n)
@@ -25,15 +28,53 @@ let testVectors(p, q, n) =
 let testBivectors(p, q, n) =
     for bv in Cl(p, q, n).Bivectors do printfn "%A" bv
 
+let testDual : (int*int*int) -> Multivector -> unit =
+    fun (p, q, n) ->
+        Cl(p, q, n).Dual >> Multivector.print
+
+let testDualInv : (int*int*int) -> Multivector -> unit =
+    fun (p, q, n) ->
+        Cl(p, q, n).DualInv >> Multivector.print
+
+let testReverse : Multivector -> unit =
+    Cl(3, 3, 2).Reverse >> Multivector.print
+
+let testGradeProject : int -> Multivector -> unit =
+    fun i m -> Cl(3, 3, 2).GradeProject i m |> Multivector.print
+
+let testGrade : Multivector -> unit =
+    Cl(3, 3, 2).Grade
+    >> fun grades -> 
+        for g in grades do Console.WriteLine g
+
+let testNeg : Multivector -> unit =
+    Cl(3, 3, 2).Neg >> Multivector.print
+
+let testAdd : Multivector -> Multivector -> unit =
+    fun a b -> Cl(3, 3, 2).Add a b |> Multivector.print
+
+let testSub : Multivector -> Multivector -> unit =
+    fun a b -> Cl(3, 3, 2).Sub a b |> Multivector.print
+
+
+let input1 = Multivector.ofBlades [
+    0b10uy, 10f
+    0b01uy, System.MathF.PI
+    0uy, 1f
+    0b11uy, -99f
+    0b11001011uy, 69f
+]
+
+let input2 = Multivector.ofBlades [
+    0b1101uy, 111f
+    0b11001011uy, 1.5f
+]
+
 
 [<EntryPoint>]
 let main _ =
-    let blades = [
-        0b10uy, 10f
-        0b01uy, System.MathF.PI
-        0uy, 1f
-        0b11uy, 99f
-    ]
-    do testMultivectorOfBlades blades
+    do
+        input1 |> testSub input2
+
 
     0
